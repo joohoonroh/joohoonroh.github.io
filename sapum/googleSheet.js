df553023a609bb3e46d51b59e74d3951181dc7d2
nick <nick@closetshare.com>
2020-08-24 10:10:24 +0900

    스크립트 파일 정리

--- sapum/googleSheet.js
+++ sapum/googleSheet.js
@@ -9,40 +9,36 @@ function doPost(e) {
 }
 
 function handleResponse(e) {
+	var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
+	var sheet = e.parameter.sheet || doc.getSheets()[0];
+	var headRow = e.parameter.header_row || 1; // 헤더 열 위치 (기본 1)
+	var headers = sheet.getRange(headRow, 1, 1, sheet.getLastColumn()).getValues()[0];
+	var target = sheet.getRange((Number(e.parameter.id) + 1), nowColumn(e.parameter.name));
+	var targetValue = target.getValue();
+	var subTargetValue = sheet.getRange((Number(e.parameter.id) + 1), nowColumn(e.parameter.sub)).getValue();
+	var func = e.parameter.func.split("self").join(targetValue).split("sub").join(subTargetValue);
+	var funcResult = eval(func);
+	var callback = funcResult == targetValue ? true : false;
 	var lock = LockService.getPublicLock();
 	lock.waitLock(30000);  // wait 30 seconds before conceding defeat.
 
-	try {
-		function nowColumn(name) {
-			for (var i = 0; i < headers.length; i++) {
-				if (headers[i] == name) {
-					return i + 1;
-				}
+	function nowColumn(name) {
+		for (var i = 0; i < headers.length; i++) {
+			if (headers[i] == name) {
+				return i + 1;
 			}
 		}
+	}
 
-		var doc = SpreadsheetApp.openById(SCRIPT_PROP.getProperty("key"));
-		var sheet = e.parameter.sheet || doc.getSheets()[0];
-		var headRow = e.parameter.header_row || 1; // 헤더 열 위치 (기본 1)
-		var headers = sheet.getRange(headRow, 1, 1, sheet.getLastColumn()).getValues()[0];
-		var target = sheet.getRange((Number(e.parameter.id) + 1), nowColumn(e.parameter.name));
-		var targetValue = target.getValue();
-		var subTargetValue = sheet.getRange((Number(e.parameter.id) + 1), nowColumn(e.parameter.sub)).getValue();
-		var func = e.parameter.func.split("self").join(targetValue).split("sub").join(subTargetValue);
-		var funcResult = eval(func);
-		var callback = funcResult == targetValue ? true : false;
-
+	try {
 		target.setValue(funcResult);
-
-		// return json success results
 		return ContentService
 			.createTextOutput(JSON.stringify({
-				"callback" : callback,
-				"callbackText" : e.parameter.callbackText
+				"callback": callback,
+				"callbackText": e.parameter.callbackText
 			}))
 			.setMimeType(ContentService.MimeType.JSON);
-	} catch (e) {
-		// if error return this
+	} catch (e) { // if error return this
 		return ContentService
 			.createTextOutput(JSON.stringify({"result": "error", "error": e}))
 			.setMimeType(ContentService.MimeType.JSON);
